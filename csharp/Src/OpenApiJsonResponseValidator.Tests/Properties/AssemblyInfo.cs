f619bf8bb29d3ab54daabe79a9d4c05b1cea51e3
lee crowe <lee.crowe@comparethemarket.com>
2021-01-25 12:07:26 +0000

    CSharp lib

---
+++ csharp/Src/OpenApiJsonResponseValidator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("13EBFD35-D162-41BD-ACA0-A7E3C1BF64A2")]
---
+++ csharp/Src/OpenApiJsonResponseValidator/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("5922AEA8-5E9C-4028-999A-7D108DDE0C66")]
