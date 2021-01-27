2f83c2506475e1b751d67d63f4732c5f3418dff3
lee crowe <lee.crowe@comparethemarket.com>
2021-01-27 10:18:31 +0000

    updating docker image

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#docker build -t croweman/openapi-json-response-validator:0.0.4 .
-#docker push croweman/openapi-json-response-validator:0.0.4
+#docker build -t croweman/openapi-json-response-validator:0.0.5 .
+#docker push croweman/openapi-json-response-validator:0.0.5
 docker-compose build --force-rm
